ea54ebdf7fde6644606affd306e3759cccde4123
ak_14 <hooploop14@gmail.com>
2017-10-21 13:17:44 +0300

    Переделал файлы конфигурации routing.yml security.yml Создал файл AuthController и twig страницу с формой авторизации

---
+++ src/AppBundle/Controller/AuthController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+
+
+
+class AuthController extends Controller
+{
+    public function loginAction(Request $request, AuthenticationUtils $authUtils)
+    {
+      $error = $authUtils->getLastAuthenticationError();
+
+//  последний авторизованный пользователь
+      $lastUsername = $authUtils->getLastUsername();
+
+//      возвращаем форму регистрации
+        return $this->render('AppBundle:Auth:login.html.twig', array(
+          'last_username' => $lastUsername,
+          'error' => $error
+        ));
+    }
+
+    public function loginCheck(Request $request){
+
+    }
+
+}
